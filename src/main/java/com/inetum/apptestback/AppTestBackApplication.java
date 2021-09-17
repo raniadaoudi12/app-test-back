14aa4b7a826a38b4779248613cf75213bc190303
raniadaoudi12 <raniadaoudi12@gmail.com>
2021-09-17 11:16:07 +0100

    Intial project creaction closes #1

---
+++ src/main/java/com/inetum/apptestback/AppTestBackApplication.java
@@ -0,0 +1,14 @@
+package com.inetum.apptestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTestBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTestBackApplication.class, args);
+		System.out.println("bonjour");
+	}
+
+}
---
+++ src/test/java/com/inetum/apptestback/AppTestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.apptestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
